3f4d0eb87d204f7a93315659e9491d3bd3ca4f44
adhemukhlis <adhemukhlis@gmail.com>
2020-08-07 13:47:52 +0700

    update 4

--- vscode-run.sh
+++ vscode-run.sh
@@ -1,1 +1,2 @@
-./code-server2.1698-vsc1.41.1-linux-x86_64/code-server --no-auth --port 8080
+export PASSWORD=123456
+./code-server2.1698-vsc1.41.1-linux-x86_64/code-server --auth password --port 8080
