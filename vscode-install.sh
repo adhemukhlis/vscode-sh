fb4b8da4f3a34f8260c44de9fe1bcd2e6f202f87
adhemukhlis <adhemukhlis@gmail.com>
2020-08-07 14:28:21 +0700

    update v3

--- vscode-install.sh
+++ vscode-install.sh
@@ -1,2 +1,2 @@
-wget https://github.com/cdr/code-server/releases/download/2.1698/code-server2.1698-vsc1.41.1-linux-x86_64.tar.gz
-tar -xvzf code-server2.1698-vsc1.41.1-linux-x86_64.tar.gz
+wget https://github.com/cdr/code-server/releases/download/3.4.1/code-server-3.4.1-linux-x86_64.tar.gz
+tar -xvzf code-server-3.4.1-linux-x86_64.tar.gz
--- vscode-run.sh
+++ vscode-run.sh
@@ -1,2 +1,2 @@
 export PASSWORD=123456
-./code-server2.1698-vsc1.41.1-linux-x86_64/code-server --auth password --port 8080
+./code-server-3.4.1-linux-x86_64/code-server --auth password --port 8080
