151e28089c9152833890760c0fe71f460c23d4ea
Mukhlis Adhe Purwanto <adhemukhlis@gmail.com>
2023-12-05 16:47:46 +0700

    Update vscode-install.sh

--- vscode-install.sh
+++ vscode-install.sh
@@ -1,2 +1,2 @@
-wget https://github.com/cdr/code-server/releases/download/3.4.1/code-server-3.4.1-linux-x86_64.tar.gz
-tar -xvzf code-server-3.4.1-linux-x86_64.tar.gz
+wget https://github.com/cdr/code-server/releases/download/3.4.1/code-server-4.19.1-linux-x86_64.tar.gz
+tar -xvzf code-server-4.19.1-linux-x86_64.tar.gz
