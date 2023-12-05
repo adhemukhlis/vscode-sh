a81407a7e1859f3bcdad0897129ac4089c8ebe93
Mukhlis Adhe Purwanto <adhemukhlis@gmail.com>
2023-12-05 16:48:16 +0700

    Update vscode-run.sh

--- vscode-run.sh
+++ vscode-run.sh
@@ -1,3 +1,3 @@
 # export PASSWORD=123456
 #  --auth password
-./code-server-3.4.1-linux-x86_64/code-server --auth none --port 8080
+./code-server-4.19.1-linux-x86_64/code-server --auth none --port 8080
